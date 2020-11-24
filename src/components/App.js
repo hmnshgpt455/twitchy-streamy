5937128a4f0ddf8aabc9fddf9d71c8a53c10239d
hmnshgpt455 <hmnshgpt455@gmail.com>
2020-11-24 08:12:48 +0530

    Converting the google auth component to Redux based and hooking up redux dev tools

--- src/components/App.js
+++ src/components/App.js
@@ -14,11 +14,11 @@ const App = () => {
          <BrowserRouter>
             <Header />
             <div>
-              <Route path="/" exact component={StreamList} />
-              <Route path="/streams/new" exact component={StreamCreate} />
-              <Route path="/streams/edit" exact component={StreamEdit} /> 
-              <Route path="/streams/delete" exact component={StreamDelete} />
-              <Route path="/streams/show" exact component={StreamShow} />
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} /> 
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="/streams/show" exact component={StreamShow} />
             </div>
          </BrowserRouter>
       </div>
--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
 
    #CLIENT_ID = '1026202403796-a855t9dnolgn3vkpdul8q205phvglsd9.apps.googleusercontent.com';
    #gapi = window.gapi;
    #auth = null;
-
-   state = {isSignedIn: null}
 
    componentDidMount() {
       this.#gapi.load('client:auth2', () => {
@@ -15,7 +16,7 @@ class GoogleAuth extends React.Component {
             scope: 'email'
          }).then(() => {
             this.#auth = this.#gapi.auth2.getAuthInstance();
-            this.setState({ isSignedIn: this.#auth.isSignedIn.get() });
+            this.onAuthChange(this.#auth.isSignedIn.get());
             this.#auth.isSignedIn.listen(this.onAuthChange);
          }).catch(err => {
             throw new Error(`Google sign in failed with message : ${err.message}`);
@@ -23,18 +24,26 @@ class GoogleAuth extends React.Component {
       });
    }
 
-   onAuthChange = () => this.setState({ isSignedIn: this.#auth.isSignedIn.get() });
+   onAuthChange = isSignedIn => { //This isSignedIn argument is passed by GAPI library itself
+      if (isSignedIn) {
+         this.props.signIn(this.#auth.currentUser.get().getId());
+      }
+
+      if (!isSignedIn) {
+         this.props.signOut();
+      }
+   };
 
    onSignInClick = () => this.#auth.signIn();
 
    onSignOutClick = () => this.#auth.signOut();
 
    renderAuthButton() {
-      if (this.state.isSignedIn === null) {
+      if (this.props.isSignedIn === null) {
          return null;
       }
 
-      if (this.state.isSignedIn) {
+      if (this.props.isSignedIn) {
          return (
             <button className="ui red google button" onClick={this.onSignOutClick}>
                <i className="google icon"/>
@@ -43,7 +52,7 @@ class GoogleAuth extends React.Component {
          );
       }
 
-      if (!this.state.isSignedIn) {
+      if (!this.props.isSignedIn) {
          return (
             <button className="ui red google button" onClick={this.onSignInClick}>
                <i className="google icon"/>
@@ -60,4 +69,11 @@ class GoogleAuth extends React.Component {
    }
 }
 
-export default GoogleAuth;
+const mapStateToProps = state => {
+   return { isSignedIn: state.auth.isSignedIn };
+}
+
+export default connect(mapStateToProps, {
+   signIn,
+   signOut
+})(GoogleAuth);
