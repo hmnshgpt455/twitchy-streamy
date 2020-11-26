82c8794ddb9500d1537f7661279760eef0405071
hmnshgpt455 <hmnshgpt455@gmail.com>
2020-11-26 09:40:56 +0530

    Small change

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div onClick={props.onDismiss} className="ui dimmer visible active">
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
       <div
         onClick={e => e.stopPropagation()}
         className="ui standard modal visible active"
